9d01b699e03735bb7a56403224861eb056daae2c
Rohit Kumar Verma <dwrsecure@gmail.com>
2025-09-15 10:45:36 +0530

    Initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
-import './index.css'
+import './index.css';
+
+// 👇 Import SpeedInsights for React
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
+        {/* 👇 Add SpeedInsights here */}
+        <SpeedInsights />
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
